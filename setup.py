97f9c9353ecb2637404e732237002bdda6089191
KirigaiKabuto <tleugazy98@gmail.com>
2020-08-12 11:24:09 +0600

    sdsdsd

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="library1",
+    version="0.0.1",
+    description="it is just library for fun",
+    author="tleugazy",
+    author_email="tleugazy98@gmail.com",
+    url="https://github.com/kirigaikabuto/PythonLibMyExample",
+    license="MIT",
+    packages=["library1"],
+    install_requires=[]
+)
